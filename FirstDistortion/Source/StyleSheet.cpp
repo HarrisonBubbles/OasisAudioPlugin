1446dacc2b58ed404e5ad47107b0f54df27b6d72
jessicaluo <luo.jes@northeastern.edu>
2021-11-07 17:03:20 -0500

    custom knob

--- FirstDistortion/Source/StyleSheet.cpp
+++ FirstDistortion/Source/StyleSheet.cpp
@@ -21,6 +21,7 @@ void juce::CustomLNF::drawRotarySlider(Graphics& g,
     static juce::Image
         img1(ImageCache::getFromFile(knobImageFile1));
 
+    if (img1.isValid()) {
         const double rotation = (slider.getValue()
             - slider.getMinimum())
             / (slider.getMaximum()
@@ -43,6 +44,33 @@ void juce::CustomLNF::drawRotarySlider(Graphics& g,
             frameId*img1.getWidth(),
             img1.getWidth(),
             img1.getWidth());
+    } else {
+        const float radius = jmin (width / 2, height / 2) - 4.0f;
+                const float centreX = x + width * 0.5f;
+                const float centreY = y + height * 0.5f;
+                const float rx = centreX - radius;
+                const float ry = centreY - radius;
+                const float rw = radius * 2.0f;
+                const float angle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
+        
+                // fill
+                g.setColour (Colours::orange);
+                g.fillEllipse (rx, ry, rw, rw);
+        
+                // outline
+                g.setColour (Colours::red);
+                g.drawEllipse (rx, ry, rw, rw, 1.0f);
+        
+                Path p;
+                const float pointerLength = radius * 0.33f;
+                const float pointerThickness = 2.0f;
+                p.addRectangle (-pointerThickness * 0.5f, -radius, pointerThickness, pointerLength);
+                p.applyTransform (AffineTransform::rotation (angle).translated (centreX, centreY));
+        
+                // pointer
+                g.setColour (Colours::yellow);
+                g.fillPath (p);
+    }
 }
 
 
